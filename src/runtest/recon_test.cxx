743a1c5247d897fbe7756ae7a091417a65d4e274
Whitney Armstrong <warmstrong@anl.gov>
2017-02-12 17:07:21 -0600

    modified:   ../../README.md 	modified:   recon_test.cxx 	deleted:    ../simple_example/src/AirTube_geo.cpp 	deleted:    ../simple_example/src/ZPlanarTracker_geo.cpp

--- src/runtest/recon_test.cxx
+++ src/runtest/recon_test.cxx
@@ -55,37 +55,16 @@ void recon_test(
   auto id      = readout.idSpec();
 
   std::cout << " num col " << readout.numCollections() << std::endl;
-  //std::cout << "id encode " << id.encode({1,0,1,3,1,0,0}) << std::endl;
   std::cout << " id spec " << id.toString() << std::endl;
   std::cout << " field "  << id.fieldDescription() << std::endl;
   std::cout << " decoder " << id.decoder() << std::endl;
-  auto bf = id.decoder();
-  //std::cout << " barrel "   << bf->index("barrel")<<std::endl;
-  //std::cout << " barrel "   << (*bf)["barrel"].name() <<std::endl;
-  //std::cout << " barrel "   << (*bf)["barrel"].offset() <<std::endl;
-  //std::cout << " barrel "   << (*bf)["barrel"].width() <<std::endl;
-  //std::cout << " barrel "   << (*bf)["barrel"].value() <<std::endl;
-
-  //auto files        = get_files_in_directory(DIRNAME);
-
-  //IO::LCReader* lcReader = IOIMPL::LCFactory::getInstance()->createLCReader() ;
-  //EVENT::LCEvent* evt = 0 ;
-  //std::string mcpType("std::vector<IMPL::MCParticleImpl*>") ;
-  //std::string mcpName("MCParticle") ;
-
-  //int  nEvents      = 0  ;
-  //std::vector<std::string> files = {"example1_ddsim_out.slcio"};
-  //std::sort(files.begin(),files.end());
 
   auto& id_decoder = DD4hep::DDRec::IDDecoder::getInstance();
 
   TFile * f = new TFile(file_name, "READ");
-  if(!f) { 
-    std::cout << " root file not found\n";  
-    exit -1;
-  }
-  f->ls();
+  if(!f) { std::cout << " root file not found\n";  exit -1; }
   TTree * t = (TTree*)f->FindObjectAny("EVENT");
+  if(!t) { std::cout << " tree not found\n";  exit -1; }
   t->Print();
 
   std::vector<DD4hep::Simulation::Geant4Tracker::Hit*> * TP_hits = nullptr;
@@ -118,80 +97,6 @@ void recon_test(
     }  
 
   }
-
-
-  //for(auto aFile : files) {
-
-  //  std::cout << "Opening " << aFile << '\n';
-  //  lcReader->open( aFile.c_str() ) ;
-  //  n_files_read++;
-
-  //  //----------- the event loop -----------
-  //  while( (evt = lcReader->readNextEvent()) != 0 ) {
-
-  //    if(nEvents%1000==0) {
-  //      if(nEvents==0)
-  //        UTIL::LCTOOLS::dumpEvent( evt ) ;
-  //      std::cout << "Event " << nEvents << '\n';
-  //    }
-
-  //    IMPL::LCCollectionVec* thrown_col = (IMPL::LCCollectionVec*) evt->getCollection( "MCParticle"  ) ;
-  //    IMPL::LCCollectionVec* simtrackhits_col = (IMPL::LCCollectionVec*) evt->getCollection("SiTrackerBarrelHits") ;
-
-  //    UTIL::LCTOOLS::printSimTrackerHits( evt->getCollection("SiTrackerBarrelHits") ); 
-
-  //    //UTIL::LCTOOLS::printMCParticles( col ) ;
-
-  //    int nMCP    = thrown_col->getNumberOfElements() ;
-  //    int nSimTrackHits = simtrackhits_col->getNumberOfElements() ;
-
-  //    CellIDDecoder<SimTrackerHit> lcio_id(simtrackhits_col);
-
-  //    for(int i_track=0 ; i_track<nSimTrackHits ; ++i_track) {
-  //      auto  ahit = (EVENT::SimTrackerHit*) simtrackhits_col->getElementAt(i_track);
-  //      //double hit_time  = ahit->getTime();
-  //      //double EDep  = ahit->getEDep();
-  //      //auto cell_id = ahit->getCellID0();
-  //      std::cout << "id " << ahit->getCellID0() << std::endl;
-
-  //      TVector3 x0(ahit->getPosition());
-  //      TVector3 x1(ahit->getPosition());
-  //      x1.Print();
-
-  //      auto hit_pos = DD4hep::Geometry::Position(x0.x(),x0.y(),x0.z());
-  //      std::cout << " hit  pos : x=" << hit_pos.x() << ", y=" << hit_pos.y() << ", z=" << hit_pos.z() << std::endl;
-
-  //      auto volumeid = id_decoder.volumeID(hit_pos);
-  //      std::cout << " volumeID : " << volumeid << std::endl;
-
-  //      //auto cellid = lcio_id(const_cast<EVENT::SimTrackerHit*>(ahit)).getValue();//id_decoder.cellID(hit_pos);
-  //      auto cellid = id_decoder.cellID(hit_pos);
-  //      std::cout << " cellID : " << cellid << std::endl;
-
-  //      auto bpos   = id_decoder.position(cellid);
-  //      std::cout << " cell pos : x=" << bpos.x() << ", y=" << bpos.y() << ", z=" << bpos.z() << std::endl;
-
-  //    }
-  //    nEvents++;
-  //  }
-  //  // -------- end of event loop -----------
-
-  //  lcReader->close() ;
-
-  //  if(n_files_read >= max_files) break;
-  //}
-
-      //auto bf      = seg.decoder();
-      //std::cout << bf->valueString() << std::endl;
-
-      //auto pos     = seg.position(1000);
-      //std::cout << " x=" << pos.x() << ", y=" << pos.y() << ", z=" << pos.z() << std::endl;
-
-  //TCanvas * c = new TCanvas();
-  //hR0->Draw();
-
-  //c = new TCanvas();
-  //hZR0->Draw("cols");
   return;
 
 }
